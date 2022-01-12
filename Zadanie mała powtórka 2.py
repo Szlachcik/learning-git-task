fc7fc36c91c84f9431f2ac5f02fc502867588521
Marcin Szlachcikowski <marcin97szlachcikowski@gmail.com>
2022-01-12 01:02:43 +0100

    Zadanie.2 3.1

---
+++ Zadanie mała powtórka 2.py
@@ -0,0 +1,13 @@
+nums = []
+for i in range(0, 101):
+    if i%5==0:
+        nums.append(i)
+print(nums)
+
+nums_ =[]
+for i in range(0, 101):
+    if i%5==0:
+        nums_.append(i**3)
+print(nums_)
+
+
